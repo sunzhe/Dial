a242ef4bdc193cf8f9b936e69ec508e9c5000baf
sunzhe <sz886@qq.com>
2012-10-09 14:37:10 +0800

    Initial Commit

---
+++ DDDial/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  DDDial
+//
+//  Created by ddmap on 12-10-9.
+//  Copyright (c) 2012年 ddmap. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ DDDial/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  DDDial
+//
+//  Created by ddmap on 12-10-9.
+//  Copyright (c) 2012年 ddmap. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
